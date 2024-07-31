dd59b9d3d6a0cacc8909fcd8ea21bc7cf456f781
rishabhrebel16 <152968729+rishabhrebel16@users.noreply.github.com>
2024-07-31 12:37:51 +0530

    Add files via upload

---
+++ p3.js
@@ -0,0 +1,66 @@
+function maskPassword(pass){
+    let str = ""
+    for (let index = 0; index < pass.length; index++) {
+        str  += "*"
+    }
+    return str
+}
+
+function copyText(txt) {
+    navigator.clipboard.writeText(txt).then(
+        () => {
+          /* clipboard successfully set */
+          document.getElementById("alert").style.display = "inline"
+          setTimeout(() => {
+            document.getElementById("alert").style.display = "none"
+          }, 2000);
+
+        },
+        () => {
+          /* clipboard write failed */
+          alert("Clipboard copying failed")
+        },
+      );
+  }
+
+  const deletePassword = (website) => {
+    let data = localStorage.getItem("passwords");
+    let arr = JSON.parse(data);
+    let arrUpdated = arr.filter((e) => {
+        return e.website !== website; 
+    });
+    localStorage.setItem("passwords", JSON.stringify(arrUpdated));
+    alert(`Successfully deleted ${website}'s password`); 
+    showPasswords(); 
+};
+// Logic to fill the table
+const showPasswords = () => {
+    let tb = document.querySelector("table")
+    let data = localStorage.getItem("passwords")
+    
+    
+}
+   
+
+console.log("Working");
+showPasswords()
+document.querySelector(".btn").addEventListener("click", (e) => {
+    e.preventDefault()
+    console.log("Clicked....")
+    console.log(username.value, password.value)
+    let passwords = localStorage.getItem("passwords")
+    console.log(passwords)
+    if (passwords == null) {
+        let json = []
+        json.push({website: website.value, username: username.value, password: password.value })
+        alert("Password Saved");
+        localStorage.setItem("passwords", JSON.stringify(json))
+    }
+    else {
+        let json = JSON.parse(localStorage.getItem("passwords"))
+        json.push({ website: website.value, username: username.value, password: password.value })
+        alert("Password Saved");
+        localStorage.setItem("passwords", JSON.stringify(json))
+    }
+    showPasswords()
+})
